d8ce875e99eb0ad9a056887f0d624736a5d65c78
闪电 <shandian145108@qq.com>
2026-09-25 11:38:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ if __name__ == "__main__":
 
     for i in range(TOTAL_RUNS):
         # 启动子进程
-        # process = subprocess.Popen(
-        #    [sys.executable, "dddh.py"],
-        #    stdin=subprocess.PIPE,
-        #    stdout=subprocess.PIPE,
-        #    stderr=subprocess.PIPE,
-        #    text=True,
-        #    bufsize=1  # 行缓冲区
-        #)
+         process = subprocess.Popen(
+            [sys.executable, "dddh.py"],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            bufsize=1  # 行缓冲区
+        )
 
         # 向子脚本写入第一行输入（回应第一个 input）
-        #process.stdin.write("17787155514\n")
-        #process.stdin.flush()
+        process.stdin.write("17787155514\n")
+        process.stdin.flush()
 
         # 获取子脚本全部输出并等待结束
-        #stdout, stderr = process.communicate()
-        #print("运行结果:\n", stdout)
-        print("Hello, World!")
+        stdout, stderr = process.communicate()
+        print("运行结果:\n", stdout)
+        #print("Hello, World!")
         if i < TOTAL_RUNS - 1:
             time.sleep(INTERVAL)
