6f7045895a245d61f26e1baa5fa78b26460eaff2
闪电 <shandian145108@qq.com>
2026-09-25 16:24:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import subprocess
 import time
 from datetime import datetime
 
-def my_task():
-    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] 执行脚本任务...")
+def dddh():
     # 启动子进程
     process = subprocess.Popen(
         [sys.executable, "dddh.py"],
@@ -22,8 +21,35 @@ def my_task():
 
     # 获取子脚本全部输出并等待结束
     stdout, stderr = process.communicate()
-    print("运行结果:\n", stdout)
+    print("dddh 运行结果:\n", stdout)
     #print("Hello, World!")
+
+def wxcy():
+    # 启动子进程
+    process = subprocess.Popen(
+        [sys.executable, "wxcy.py"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        bufsize=1  # 行缓冲区
+    )
+
+    process.stdin.write("2\n")
+    process.stdin.flush()
+
+    process.stdin.write("17787155514\n")
+    process.stdin.flush()
+
+    # 获取子脚本全部输出并等待结束
+    stdout, stderr = process.communicate()
+    print("wxcy运行结果:\n", stdout)
+    #print("Hello, World!")
+
+def my_task():
+    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] 执行脚本任务...")
+    dddh()
+    wxcy()
 
 if __name__ == "__main__":
     INTERVAL = 15   # 间隔 15 秒
